eab9c5276f8d9997d66e2859649d632359a46827
saylik <saylikadam16@gmail.com>
2015-05-06 14:48:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saylik/iteration/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.saylik.iteration;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button recordEstimateButton = (Button)findViewById(R.id.rec_est_button);
+        recordEstimateButton
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
